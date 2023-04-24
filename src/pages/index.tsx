5eb8a8b437a577eae2c22c0f8b7d2ab22ef28314
darkstorn <inikishin@gmail.com>
2023-04-24 16:11:21 +0300

    update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 import { HeroSection } from "@/components/sections/hero-section/hero-section";
 
 export default function Home() {
