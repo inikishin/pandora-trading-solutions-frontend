85990b0ea6aba6a87910ea5519a87b810e065dd3
darkstorn <inikishin@gmail.com>
2023-04-25 08:06:28 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
+const commonRuntimeConfig = {
+  ENV: process.env.NODE_ENV || 'development',
+  API_URL: process.env.API_URL || 'https://ecosystem.osinit.net/api/v1',
+};
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  env: commonRuntimeConfig,
+  poweredByHeader: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   experimental: {
-    appDir: true,
-  }
+    appDir: false,
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
