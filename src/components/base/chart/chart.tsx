a5f6967ec306103d03e4a901bc6b7656a063e164
darkstorn <inikishin@gmail.com>
2023-04-25 08:05:50 +0300

    add Chart component

---
+++ src/components/base/chart/chart.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef } from "react";
+import { createChart, IChartApi } from 'lightweight-charts';
+
+type QuoteType = {
+  time: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+}
+type ChartType = {
+  data: Array<QuoteType>;
+};
+
+export const Chart: React.FC<ChartType> = ({ data }) => {
+  const chartContainerRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(
+		() => {
+      let chart: IChartApi | undefined = undefined;
+
+      const handleResize = () => {
+        if (chart && chartContainerRef && chartContainerRef.current) {
+          chart.applyOptions({width: chartContainerRef.current.clientWidth});
+        }
+      };
+
+      if (chartContainerRef && chartContainerRef.current) {
+        chart = createChart(chartContainerRef.current);
+        const mainSeries = chart.addCandlestickSeries();
+        mainSeries.setData(data);
+
+        chart.timeScale().fitContent();
+      }
+			window.addEventListener('resize', handleResize);
+
+			return () => {
+				window.removeEventListener('resize', handleResize);
+        if (chart) {
+          chart.remove();
+        }
+			};
+		},
+		[data]
+	);
+
+  return (
+    <div className="w-full h-full" ref={chartContainerRef} />
+  );
+};
