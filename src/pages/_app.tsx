bd785e67b67e4b14556ea71833c0899e8fc414d6
darkstorn <inikishin@gmail.com>
2023-04-25 10:54:39 +0300

    add redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css';
+import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 
+import { wrapper } from '@/store';
+import '@/styles/globals.css';
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { store, props } = wrapper.useWrappedStore({ ...pageProps });
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
