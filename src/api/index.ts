eda5956e846dbeb7fd20f8fe8f11e06dad296262
darkstorn <inikishin@gmail.com>
2023-05-25 16:22:00 +0300

    add screener api instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import adminApi from "@/api/admin";
 import quotesApi from "@/api/quotes";
+import screenerApi from "@/api/screener";
 
 const api = {
   admin: adminApi,
   quotes: quotesApi,
+  screener: screenerApi,
 };
 
 export default api;
